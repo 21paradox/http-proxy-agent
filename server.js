8028505990d8dd62ed440c2e0259e322b3da07b1
unknown <>
2016-11-13 12:47:06 +0800

    proxy

--- server.js
+++ server.js
@@ -1,73 +1,38 @@
-var httpProxy = require('http-proxy');
 var http = require('http');
-var url = require('url');
-var https = require('https');
-var fs = require('fs');
- 
-var port = process.env.PORT || 8444;
- 
-var parseHostHeader = function (headersHost, defaultPort) {
-    var hostAndPort = headersHost.split(':'),
-        targetHost = hostAndPort[0],
-        targetPort = parseInt(hostAndPort[1]) || defaultPort;
-
-    return { hostname: targetHost, port: targetPort, host: headersHost };
-}
-
-var adjustRequestUrl = function (req) {
-    //if (requestToProxyMirrorWebApp(req)) {
-    //    req.url = req.url.replace(/http:\/\/localhost:8889\//, '/');
-    //}
-    req.url = url.parse(req.url).path;
-};
-
-//var proxy = httpProxy.createProxyServer({
-//    changeOrigin: true,
-//    xfwd: false,
-//    secure: true
-//});
-
-
-http.createServer(function (req, res) {
-   
-    //var parsedHostHeader = parseHostHeader(req.headers['host'], 80);
-
-    //console.log(req.headers['host']);
-
-    //console.log(req.host);
-
-    //console.log(req.url);
-
-    adjustRequestUrl(req);
-
-    //console.log(req.url);
-
-   // proxy.web(req, res, { target: 'http://127.0.0.1:5060' });
-
-    var protocal;
- 
-    if (req.url.match(/^https/)) {
-
-        protocal = 'https://'
-
-    } else if (req.url.match(/^http/)) {
-        protocal = 'http://'
-    }
-
-    proxy.web(req, res, { target: 'http://' + req.headers['host'] });
-   
-})
-
-    //.listen(9000);
-
- 
-
 var setup = require('proxy');
+var express = require('express');
 
+
+var port = process.env.PORT || 18080;
 var server = setup(http.createServer());
+
+/* 提取原有的 onRequest */
+var onRequest = server.listeners('request')[0];
+server.removeListener('request', onRequest);
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.end('hello world');
+});
+
+server.on('request', function (req, res) {
+
+    console.log(req.url);
+
+    if (req.url.indexOf('/') === 0) {
+        app(req, res);
+    } else {
+        onRequest(req, res);
+    }
+});
+
+
+server.on('connect', function(req, cltSocket, head) {
+    console.log(req);
+})
 
 
 server.listen(port, function () {
-    var port = server.address().port;
-    console.log('HTTP(s) proxy server listening on port %d', port);
+    console.log('listening on port %d', port);
 });
